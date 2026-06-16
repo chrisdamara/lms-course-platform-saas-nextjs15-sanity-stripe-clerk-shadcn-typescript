539e1303b12237450f64308a27d65f5ba2bc5bd4
chrisdamara <cpritchardcairns1@gmail.com>
2026-06-16 21:43:21 +0700

    nn

--- lib/baseUrl.ts
+++ lib/baseUrl.ts
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-    : `${process.env.NEXT_PUBLIC_BASE_URL}`;
+    ? `https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL
+    : process.env.NEXT_PUBLIC_BASE_URL || "https://lms-course-platform-saas-nextjs15-s-orpin.vercel.app/";
 
 export default baseUrl;
