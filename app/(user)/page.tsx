ac03dedc35eaa778b56592eb6fa063cb64ea95eb
chrisdamara <cpritchardcairns1@gmail.com>
2026-06-16 11:34:37 +0700

    Update page.tsx

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -103,7 +103,7 @@ export default async function Home() {
             <div className="relative">
               <Quote className="absolute -top-6 -left-6 h-12 w-12 text-primary/10" />
               <p className="text-2xl md:text-3xl font-medium tracking-tight text-foreground relative z-10 italic">
-                &quot;Damara Surf Tutorials transformed my popup technique in less than a week. The video analysis and detailed wave dynamics lectures are second to none!&quot;
+                'Damara Surf Tutorials transformed my popup technique in less than a week. The video analysis and detailed wave dynamics lectures are second to none!'
               </p>
             </div>
 
